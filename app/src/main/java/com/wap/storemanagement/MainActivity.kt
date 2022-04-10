192c2bb765dc00be39f6ffbf345c18138d58dc27
차지원 <chajiwon100785@gmail.com>
2022-04-10 18:40:05 +0900

    Feat: 수정 main activity에 base activity 상속 [#10]

--- app/src/main/java/com/wap/storemanagement/MainActivity.kt
+++ app/src/main/java/com/wap/storemanagement/MainActivity.kt
@@ -1,21 +1,8 @@
 package com.wap.storemanagement
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
+import com.wap.base.BaseActivity
 import com.wap.storemanagement.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
-class MainActivity: AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
-    }
-}
+class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main)
