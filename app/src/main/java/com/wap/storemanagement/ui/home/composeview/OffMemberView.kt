a523475c01ab81d3bd314436b2bd9ec2f5d10a72
Kongbee <rhdmsqls5@gmail.com>
2022-04-14 19:48:30 +0900

    Layout: 추가 OffMemberView class [#17]

--- app/src/main/java/com/wap/storemanagement/ui/home/composeview/OffMemberView.kt
+++ app/src/main/java/com/wap/storemanagement/ui/home/composeview/OffMemberView.kt
@@ -1,15 +1,31 @@
 package com.wap.storemanagement.ui.home.composeview
 
+import android.content.Context
+import android.util.AttributeSet
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.AbstractComposeView
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.wap.storemanagement.R
+
+class OffMemberView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+): AbstractComposeView(context, attrs, defStyle) {
+
+    @Composable
+    override fun Content() {
+        OffMemberTitle()
+        OffMemberLazyList()
+    }
+}
 
 @Composable
 fun OffMemberTitle() {
