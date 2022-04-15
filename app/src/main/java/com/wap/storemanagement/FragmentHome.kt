9b2979a48bb1aa1f2dd141d853c23ab6c026386e
Kongbee <rhdmsqls5@gmail.com>
2022-04-15 16:31:14 +0900

    Feat: 생성 FragmentHome.kt [#17]

---
+++ app/src/main/java/com/wap/storemanagement/FragmentHome.kt
@@ -0,0 +1,17 @@
+package com.wap.storemanagement
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.wap.base.BaseFragment
+import com.wap.storemanagement.databinding.FragmentHomeBinding
+
+class FragmentHome: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
