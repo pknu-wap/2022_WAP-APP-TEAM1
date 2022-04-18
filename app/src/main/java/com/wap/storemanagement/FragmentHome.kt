413d4dcd6d5e97bc37bb50a416c9cb5e6faede8e
Kongbee <rhdmsqls5@gmail.com>
2022-04-18 21:37:49 +0900

    Feat: 추가 Listener - 날짜 선택 [#21]

--- app/src/main/java/com/wap/storemanagement/FragmentHome.kt
+++ app/src/main/java/com/wap/storemanagement/FragmentHome.kt
@@ -2,23 +2,43 @@ package com.wap.storemanagement
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.wap.base.BaseFragment
 import com.wap.storemanagement.databinding.FragmentHomeBinding
+import com.wap.storemanagement.ui.home.ScheduleViewModel
 import com.wap.storemanagement.ui.home.composeview.OffMemberView
-import com.wap.storemanagement.ui.home.composeview.ScheduleCard
+import com.wap.storemanagement.ui.home.composeview.ScheduleCards
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FragmentHome: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    private val viewModel: ScheduleViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.composeHomeSchedule.setContent {
-            ScheduleCard("", "")
-        }
-
         binding.composeHomeOffMember.setContent {
             OffMemberView()
         }
+
+        setScheduleView()
+
+        setDateClickListener()
     }
 
+    private fun setScheduleView() {
+        viewModel.schedules.observe(requireActivity()) { schedules ->
+            binding.composeHomeSchedule.setContent {
+                ScheduleCards(schedules = schedules)
+            }
+        }
+    }
+
+    private fun setDateClickListener() {
+        binding.calendarHomeCalendar.setOnDateChangedListener{ widget, date, selected ->
+            if(selected)
+                viewModel.fetchSchedules(date)
+        }
+    }
 }
