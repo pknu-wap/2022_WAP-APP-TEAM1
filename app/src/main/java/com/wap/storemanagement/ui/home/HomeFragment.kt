89b12d45eba0520789d0513024cff52b452cdcec
Kongbee <rhdmsqls5@gmail.com>
2022-04-18 21:42:54 +0900

    Rename: 변경 HomeFragment - 이름, 위치 [#21]

--- app/src/main/java/com/wap/storemanagement/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wap/storemanagement/ui/home/HomeFragment.kt
@@ -1,17 +1,17 @@
-package com.wap.storemanagement
+package com.wap.storemanagement.ui.home
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.wap.base.BaseFragment
+import com.wap.storemanagement.R
 import com.wap.storemanagement.databinding.FragmentHomeBinding
-import com.wap.storemanagement.ui.home.ScheduleViewModel
 import com.wap.storemanagement.ui.home.composeview.OffMemberView
 import com.wap.storemanagement.ui.home.composeview.ScheduleCards
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class FragmentHome: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class HomeFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
     private val viewModel: ScheduleViewModel by viewModels()
 
