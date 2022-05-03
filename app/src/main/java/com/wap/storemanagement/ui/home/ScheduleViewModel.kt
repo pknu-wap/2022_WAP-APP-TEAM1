5e3e68a6842c8c156a05a933913258b2c405600b
Kongbee <rhdmsqls5@gmail.com>
2022-05-03 15:54:40 +0900

    Feat: 수정 ScheduleViewModel - fetchSchedules 함수 [#24]

--- app/src/main/java/com/wap/storemanagement/ui/home/ScheduleViewModel.kt
+++ app/src/main/java/com/wap/storemanagement/ui/home/ScheduleViewModel.kt
@@ -5,21 +5,21 @@ import androidx.lifecycle.MutableLiveData
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.wap.base.BaseViewModel
 import com.wap.base.provider.DispatcherProvider
-import com.wap.storemanagement.data.entity.ScheduleEntity
-import com.wap.storemanagement.fake.FakeFactory
+import com.wap.storemanagement.data.entity.Schedule
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ScheduleViewModel @Inject constructor(dispatcherProvider: DispatcherProvider): BaseViewModel(dispatcherProvider) {
 
-    private var _schedules: MutableLiveData<List<ScheduleEntity>> = MutableLiveData()
-    val schedules: LiveData<List<ScheduleEntity>> = _schedules
+    private var _schedules: MutableLiveData<List<Schedule>> = MutableLiveData()
+    val schedules: LiveData<List<Schedule>> = _schedules
 
     //TODO : 달력에서 날짜를 선택했을 때 날짜에 해당하는 모든 일정을 반환
     //FIXME : 날짜 자료형 확정
     fun fetchSchedules(date: CalendarDay) {
         //TODO : repository에 요청
-        _schedules.value = FakeFactory.createSchedules()
+        //_schedules.value = FakeFactory.createSchedules()
+        //FIXME : Fake 다시 생성후 사용
     }
 }
