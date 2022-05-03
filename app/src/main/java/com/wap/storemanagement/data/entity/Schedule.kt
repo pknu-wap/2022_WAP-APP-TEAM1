09c4b960baefee04c0fef8af8d7764d4754ceb1a
Kongbee <rhdmsqls5@gmail.com>
2022-05-03 15:50:55 +0900

    Feat: 수정 Schedule Entity [#24]

---
+++ app/src/main/java/com/wap/storemanagement/data/entity/Schedule.kt
@@ -0,0 +1,22 @@
+package com.wap.storemanagement.data.entity
+
+import java.time.LocalDateTime
+
+data class Schedule(
+    val scheduleId: Long, // 기본키
+    val startTime: LocalDateTime,
+    val endTime: LocalDateTime,
+    val color: String,
+    val recurWeek: WeekType? = null,
+    val userId: Long, // 외래키
+)
+
+enum class WeekType {
+    SUN,
+    MON,
+    TUE,
+    WED,
+    THU,
+    FRI,
+    SAT
+}
