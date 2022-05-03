d6fd46d665af76ad5f2fe0f84db63d474da97c5e
Kongbee <rhdmsqls5@gmail.com>
2022-05-03 15:38:16 +0900

    Feat: 추가 FragmentManager - MainActivity [#21]

--- app/src/main/java/com/wap/storemanagement/MainActivity.kt
+++ app/src/main/java/com/wap/storemanagement/MainActivity.kt
@@ -1,8 +1,22 @@
 package com.wap.storemanagement
 
+import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.wap.base.BaseActivity
 import com.wap.storemanagement.databinding.ActivityMainBinding
+import com.wap.storemanagement.ui.home.HomeFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main)
+class MainActivity: BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager.commit {
+            replace<HomeFragment>(R.id.fragmentContainer_main_fragment)
+        }
+    }
+
+}
