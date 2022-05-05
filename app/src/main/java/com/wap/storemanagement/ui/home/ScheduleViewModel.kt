d37ee9036cdceb2d867c6cd26a98c238bb312653
Kongbee <rhdmsqls5@gmail.com>
2022-05-05 18:25:56 +0900

    Feat: 추가 fetchSchedules - 선택된 날짜의 일정 List 생성 [#27]

--- app/src/main/java/com/wap/storemanagement/ui/home/ScheduleViewModel.kt
+++ app/src/main/java/com/wap/storemanagement/ui/home/ScheduleViewModel.kt
@@ -1,25 +1,49 @@
 package com.wap.storemanagement.ui.home
 
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.wap.base.BaseViewModel
 import com.wap.base.provider.DispatcherProvider
 import com.wap.storemanagement.data.entity.Schedule
+import com.wap.storemanagement.fake.FakeFactory
 import dagger.hilt.android.lifecycle.HiltViewModel
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
 class ScheduleViewModel @Inject constructor(dispatcherProvider: DispatcherProvider): BaseViewModel(dispatcherProvider) {
 
     private var _schedules: MutableLiveData<List<Schedule>> = MutableLiveData()
-    val schedules: LiveData<List<Schedule>> = _schedules
+    val currentDateSchedules:MutableLiveData<List<Schedule>> = MutableLiveData()
 
     //TODO : 달력에서 날짜를 선택했을 때 날짜에 해당하는 모든 일정을 반환
     //FIXME : 날짜 자료형 확정
+    @RequiresApi(Build.VERSION_CODES.O)
     fun fetchSchedules(date: CalendarDay) {
         //TODO : repository에 요청
-        //_schedules.value = FakeFactory.createSchedules()
-        //FIXME : Fake 다시 생성후 사용
+        var currentScheduleList: MutableList<Schedule> = mutableListOf()
+        _schedules.value = FakeFactory.createSchedules()
+
+        _schedules.value?.forEach { schedule ->
+            if (isCurrentDateSchedule(date, schedule)){
+                currentScheduleList.add(schedule)
+            }
+
+        currentDateSchedules.value = currentScheduleList
+
+        }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun isCurrentDateSchedule(date: CalendarDay, schedule: Schedule) = date.toDate() == schedule.startTime.toScheduleDate()
+    //FIXME : 캘린더 월이 1달씩 밀림   ex.May 5 클릭 -> Log : 4월 5일
 }
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun LocalDateTime.toScheduleDate() = format(DateTimeFormatter.ofPattern("yyyyMMdd"))
+fun CalendarDay.toDate() = hashCode().toString()
