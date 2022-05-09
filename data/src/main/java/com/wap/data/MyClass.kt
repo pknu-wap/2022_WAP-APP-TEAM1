2d9299217da0c44c45ac2a1775a7201aa13f059d
Kongbee <rhdmsqls5@gmail.com>
2022-05-09 23:10:00 +0900

    Chore: 변경 data module Library - kotlin -> android [#32]

--- data/src/main/java/com/wap/data/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.wap.data
-
-class MyClass {
-}
--- data/src/main/java/com/wap/data/local/ScheduleLocalDataSourceImpl.kt
+++ data/src/main/java/com/wap/data/local/ScheduleLocalDataSourceImpl.kt
@@ -1,4 +1,37 @@
 package com.wap.data.local
 
-class ScheduleLocalDataSourceImpl {
+import com.wap.domain.entity.Schedule
+import com.wap.domain.entity.WeekType
+import com.wap.domain.local.ScheduleLocalDataSource
+import java.time.LocalDateTime
+
+class ScheduleLocalDataSourceImpl : ScheduleLocalDataSource {
+
+    override fun getSchedule(scheduleId: Long): Schedule {
+        TODO("Not yet implemented")
+    }
+
+    override fun createSchedule(schedule: Schedule) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateStartTime(date: LocalDateTime) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateEndTime(date: LocalDateTime) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateColor(color: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateRecurWeek(weekType: WeekType) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteSchedule(scheduleId: Long) {
+        TODO("Not yet implemented")
+    }
 }
