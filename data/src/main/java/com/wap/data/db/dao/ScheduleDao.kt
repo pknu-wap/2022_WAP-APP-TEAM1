2d9f89bc22844ed93ec296eae03d380439223736
Kongbee <rhdmsqls5@gmail.com>
2022-05-11 21:05:23 +0900

    Comment: 추가 구분용 주석 [#35]

--- data/src/main/java/com/wap/data/db/dao/ScheduleDao.kt
+++ data/src/main/java/com/wap/data/db/dao/ScheduleDao.kt
@@ -6,18 +6,25 @@ import com.wap.data.entity.ScheduleEntity
 @Dao
 interface ScheduleDao {
 
+    // Insert
     @Insert
     fun insertSchedule(schedule: ScheduleEntity)
 
+
+    // Find
     @Query("SELECT * FROM SCHEDULE WHERE user_id = :userId")
     fun findSchedulesByUserId(userId: Long): List<ScheduleEntity>?
 
     @Query("SELECT * FROM SCHEDULE WHERE startTime = :startTime")
     fun findSchedulesByStartDate(startTime: Long): List<ScheduleEntity>?
 
+
+    // Update
     @Update
     fun updateSchedule(schedule: ScheduleEntity)
 
+    
+    // Delete
     @Query("DELETE FROM SCHEDULE WHERE user_id = :userId")
     fun deleteSchedulesByUserId(userId: Long)
 
