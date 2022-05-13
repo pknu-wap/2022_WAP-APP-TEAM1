71ea126a2129f335dee527578066896b7135808b
Kongbee <rhdmsqls5@gmail.com>
2022-05-14 00:25:19 +0900

    Feat: 추가 AddScheduleActivity [#47]

---
+++ app/src/main/java/com/wap/storemanagement/ui/schedule/AddScheduleActivity.kt
@@ -0,0 +1,40 @@
+package com.wap.storemanagement.ui.schedule
+
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import com.wap.base.BaseActivity
+import com.wap.storemanagement.R
+import com.wap.storemanagement.databinding.ActivityScheduleBinding
+import com.wap.storemanagement.fake.FakeFactory
+import com.wap.storemanagement.ui.schedule.composeview.AddScheduleTopAppBar
+import com.wap.storemanagement.ui.schedule.composeview.CheckDateView
+import com.wap.storemanagement.ui.schedule.composeview.SaveButton
+import com.wap.storemanagement.ui.schedule.composeview.ScheduleView
+import java.time.LocalDateTime
+
+@RequiresApi(Build.VERSION_CODES.O)
+class AddScheduleActivity: BaseActivity<ActivityScheduleBinding>(R.layout.activity_schedule) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        binding.composeScheduleTopAppbar.setContent { AddScheduleTopAppBar() }
+        SetCheckDateView()
+        SetScrollScheduleView()
+        binding.composeScheduleSaveButton.setContent { SaveButton() }
+    }
+
+    private fun SetCheckDateView() {
+        binding.composeScheduleCheckDate.setContent {
+            // FIXME : ViewModel 작성 후 연결
+            CheckDateView(selectDay = LocalDateTime.of(2222,2,22,22,2))
+        }
+    }
+
+    private fun SetScrollScheduleView() {
+        // FIXME : ViewModel 작성 후 연결
+        binding.composeScheduleScrollSchedule.setContent {
+            ScheduleView(schedules = FakeFactory.createSchedules()) }
+    }
+}
