448887dc6f017dd294e70f9ad756bf6189c7c39c
Kongbee <rhdmsqls5@gmail.com>
2022-05-14 18:57:07 +0900

    Layout: 추가 SetScheduleCalendar Activiy, xml [#53]

---
+++ app/src/main/java/com/wap/storemanagement/ui/set/SetScheduleCalendarActivity.kt
@@ -0,0 +1,8 @@
+package com.wap.storemanagement.ui.set
+
+import com.wap.base.BaseActivity
+import com.wap.storemanagement.R
+import com.wap.storemanagement.databinding.ActivitySetBinding
+
+class SetScheduleCalendarActivity : BaseActivity<ActivitySetBinding>(R.layout.activity_set) {
+}
