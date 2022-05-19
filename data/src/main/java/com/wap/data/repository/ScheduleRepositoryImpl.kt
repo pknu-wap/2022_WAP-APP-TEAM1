c11cc330215972cda7623606e838f6f5165781b0
Kongbee <rhdmsqls5@gmail.com>
2022-05-19 17:55:03 +0900

    Feat: 추가 schedule data source를 상속한repository [#61]

---
+++ data/src/main/java/com/wap/data/repository/ScheduleRepositoryImpl.kt
@@ -0,0 +1,56 @@
+package com.wap.data.repository
+
+import com.wap.domain.entity.Schedule
+import com.wap.domain.entity.WeekType
+import com.wap.domain.datasource.ScheduleDataSource
+import java.time.LocalDateTime
+import javax.inject.Inject
+
+class ScheduleRepository @Inject constructor(
+    val scheduleDataSource: ScheduleDataSource
+) : ScheduleDataSource {
+
+    private var _currentDateSchedules: List<Schedule> = emptyList()
+    val currentDateSchedules = _currentDateSchedules
+
+    fun saveCurrentDateSchedules(currentDateSchedules: List<Schedule>) {
+        _currentDateSchedules = currentDateSchedules
+    }
+
+    override fun getSchedule(scheduleId: Long): Schedule {
+        TODO("Not yet implemented")
+    }
+
+    override fun findSchedulesByUserId(userId: Long): List<Schedule> {
+        TODO("Not yet implemented")
+    }
+
+    override fun findSchedulesByStartTime(startTime: LocalDateTime): List<Schedule> {
+        TODO("Not yet implemented")
+    }
+
+    override fun createSchedule(schedule: Schedule) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateStartTime(date: LocalDateTime) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateEndTime(date: LocalDateTime) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateColor(color: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateRecurWeek(weekType: WeekType) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteSchedule(scheduleId: Long) {
+        TODO("Not yet implemented")
+    }
+
+}
