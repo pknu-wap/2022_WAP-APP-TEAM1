5425d26104ff4ff711700425d7889cdb2a55787a
Kongbee <rhdmsqls5@gmail.com>
2022-05-19 20:36:30 +0900

    Feat: 추가 ScheduleRepository - current Date, Schedules [#61]

--- data/src/main/java/com/wap/data/repository/ScheduleRepository.kt
+++ data/src/main/java/com/wap/data/repository/ScheduleRepository.kt
@@ -1,20 +1,30 @@
 package com.wap.data.repository
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.wap.domain.entity.Schedule
 import com.wap.domain.entity.WeekType
 import com.wap.domain.datasource.ScheduleDataSource
 import java.time.LocalDateTime
 import javax.inject.Inject
 
+@RequiresApi(Build.VERSION_CODES.O)
 class ScheduleRepository @Inject constructor(
     val scheduleDataSource: ScheduleDataSource
 ) : ScheduleDataSource {
 
+    private var _currentDate: LocalDateTime = LocalDateTime.now()
+    val currentDate get() = _currentDate
+
     private var _currentDateSchedules: List<Schedule> = emptyList()
-    val currentDateSchedules = _currentDateSchedules
+    val currentDateSchedules get() = _currentDateSchedules
 
     fun saveCurrentDateSchedules(currentDateSchedules: List<Schedule>) {
         _currentDateSchedules = currentDateSchedules
+    }
+
+    fun saveCurrentDate(currentDate: LocalDateTime) {
+        _currentDate = currentDate
     }
 
     override fun getSchedule(scheduleId: Long): Schedule {
