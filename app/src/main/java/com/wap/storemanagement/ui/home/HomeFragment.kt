2490ac26abf8444423bddede11712b741db5a1f1
Kongbee <rhdmsqls5@gmail.com>
2022-05-19 15:18:59 +0900

    Feat: 추가 setScheduleView - startActivity [#61]

--- app/src/main/java/com/wap/storemanagement/ui/home/HomeFragment.kt
+++ app/src/main/java/com/wap/storemanagement/ui/home/HomeFragment.kt
@@ -1,15 +1,20 @@
 package com.wap.storemanagement.ui.home
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.viewModels
 import com.wap.base.BaseFragment
 import com.wap.storemanagement.R
 import com.wap.storemanagement.databinding.FragmentHomeBinding
 import com.wap.storemanagement.ui.home.composeview.OffMemberView
 import com.wap.storemanagement.ui.home.composeview.ScheduleCards
+import com.wap.storemanagement.ui.schedule.AddEditScheduleActivity
 import dagger.hilt.android.AndroidEntryPoint
 
+@RequiresApi(Build.VERSION_CODES.O)
 @AndroidEntryPoint
 class HomeFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
@@ -28,9 +33,13 @@ class HomeFragment: BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     }
 
     private fun setScheduleView() {
+        val intent = Intent(context, AddEditScheduleActivity::class.java)
+
         viewModel.currentDataSchedules.observe(requireActivity()) { schedules ->
             binding.composeHomeSchedule.setContent {
-                ScheduleCards(schedules = schedules)
+                ScheduleCards(schedules = schedules) {
+                    startActivity(intent)
+                }
             }
         }
     }
