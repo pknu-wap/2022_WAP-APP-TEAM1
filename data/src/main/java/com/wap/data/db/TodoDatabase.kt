14e617993fa2207c1d2727b63dbe0eaf63f775e9
Daaeeuun <eun000221@gmail.com>
2022-05-23 17:40:40 +0900

    Feat: 추가 TodoDatabase #74

---
+++ data/src/main/java/com/wap/data/db/TodoDatabase.kt
@@ -0,0 +1,11 @@
+package com.wap.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.wap.data.db.dao.TodoDao
+import com.wap.domain.entity.ToDo
+
+@Database(entities = arrayOf(ToDo::class), version = 1)
+abstract class TodoDatabase: RoomDatabase() {
+    abstract fun TodoDao(): TodoDao
+}
