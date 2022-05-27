8f8df8dba5ccf20c90667e6c323588906903bcce
차지원 <chajiwon100785@gmail.com>
2022-05-28 00:56:27 +0900

    Feat: 적용 todo adapter [#82]

--- app/src/main/java/com/wap/storemanagement/ui/todo/ToDoFragment.kt
+++ app/src/main/java/com/wap/storemanagement/ui/todo/ToDoFragment.kt
@@ -1,9 +1,24 @@
 package com.wap.storemanagement.ui.todo
 
+import android.os.Bundle
+import android.view.View
 import com.wap.base.BaseFragment
 import com.wap.storemanagement.R
 import com.wap.storemanagement.databinding.FragmentTodoBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ToDoFragment : BaseFragment<FragmentTodoBinding>(R.layout.fragment_todo)
+class ToDoFragment : BaseFragment<FragmentTodoBinding>(R.layout.fragment_todo) {
+
+    private val toDoAdapter by lazy { ToDoAdapter() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+        binding.recyclerViewTodo.adapter = toDoAdapter
+    }
+}
