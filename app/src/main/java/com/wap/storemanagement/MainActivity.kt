f084a0ae4ec5ecdf8080b46bfa6d949d117733b2
차지원 <chajiwon100785@gmail.com>
2022-05-27 23:24:01 +0900

    Feat: 추가 navigation [#79]

--- app/src/main/java/com/wap/storemanagement/MainActivity.kt
+++ app/src/main/java/com/wap/storemanagement/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.wap.storemanagement
 
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.wap.base.BaseActivity
 import com.wap.storemanagement.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -7,42 +10,15 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private fun initBottomNavigation() {
+    private val navController by lazy { findNavController(R.id.container_main) }
 
-        // supportFragmentManager.beginTransaction()
-        //     .replace(R.id.main_frm, HomeFragment())
-        //     .commitAllowingStateLoss()
-        //
-        // binding.mainBnv.setOnItemSelectedListener{ item ->
-        //     when (item.itemId) {
-        //
-        //         R.id.homeFragment -> {
-        //             supportFragmentManager.beginTransaction()
-        //                 .replace(R.id.main_frm, HomeFragment())
-        //                 .commitAllowingStateLoss()
-        //             return@setOnItemSelectedListener true
-        //         }
-        //
-        //         R.id.lookFragment -> {
-        //             supportFragmentManager.beginTransaction()
-        //                 .replace(R.id.main_frm, LookFragment())
-        //                 .commitAllowingStateLoss()
-        //             return@setOnItemSelectedListener true
-        //         }
-        //         R.id.searchFragment -> {
-        //             supportFragmentManager.beginTransaction()
-        //                 .replace(R.id.main_frm, SearchFragment())
-        //                 .commitAllowingStateLoss()
-        //             return@setOnItemSelectedListener true
-        //         }
-        //         R.id.lockerFragment -> {
-        //             supportFragmentManager.beginTransaction()
-        //                 .replace(R.id.main_frm, LockerFragment())
-        //                 .commitAllowingStateLoss()
-        //             return@setOnItemSelectedListener true
-        //         }
-        //     }
-        //     false
-        // }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setNavHostFragment()
+    }
+
+    private fun setNavHostFragment() {
+        binding.bottomBarMain.setupWithNavController(navController)
     }
 }
