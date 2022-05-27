463086f5b05198218ecd4b1468df0a65a6bab9b4
차지원 <chajiwon100785@gmail.com>
2022-05-27 22:34:02 +0900

    Fix: 주석처리 빌드에러 수정 [#79]

--- app/src/main/java/com/wap/storemanagement/MainActivity.kt
+++ app/src/main/java/com/wap/storemanagement/MainActivity.kt
@@ -1,50 +1,48 @@
 package com.wap.storemanagement
 
-import android.os.Bundle
-import android.view.MenuItem
 import com.wap.base.BaseActivity
 import com.wap.storemanagement.databinding.ActivityMainBinding
-import com.wap.storemanagement.ui.home.HomeFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
-    private fun initBottomNavigation(){
+    private fun initBottomNavigation() {
 
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_frm, HomeFragment())
-            .commitAllowingStateLoss()
-
-        binding.mainBnv.setOnItemSelectedListener{ item ->
-            when (item.itemId) {
-
-                R.id.homeFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, HomeFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.lookFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, LookFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.searchFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, SearchFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.lockerFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, LockerFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-            }
-            false
-        }
+        // supportFragmentManager.beginTransaction()
+        //     .replace(R.id.main_frm, HomeFragment())
+        //     .commitAllowingStateLoss()
+        //
+        // binding.mainBnv.setOnItemSelectedListener{ item ->
+        //     when (item.itemId) {
+        //
+        //         R.id.homeFragment -> {
+        //             supportFragmentManager.beginTransaction()
+        //                 .replace(R.id.main_frm, HomeFragment())
+        //                 .commitAllowingStateLoss()
+        //             return@setOnItemSelectedListener true
+        //         }
+        //
+        //         R.id.lookFragment -> {
+        //             supportFragmentManager.beginTransaction()
+        //                 .replace(R.id.main_frm, LookFragment())
+        //                 .commitAllowingStateLoss()
+        //             return@setOnItemSelectedListener true
+        //         }
+        //         R.id.searchFragment -> {
+        //             supportFragmentManager.beginTransaction()
+        //                 .replace(R.id.main_frm, SearchFragment())
+        //                 .commitAllowingStateLoss()
+        //             return@setOnItemSelectedListener true
+        //         }
+        //         R.id.lockerFragment -> {
+        //             supportFragmentManager.beginTransaction()
+        //                 .replace(R.id.main_frm, LockerFragment())
+        //                 .commitAllowingStateLoss()
+        //             return@setOnItemSelectedListener true
+        //         }
+        //     }
+        //     false
+        // }
     }
 }
