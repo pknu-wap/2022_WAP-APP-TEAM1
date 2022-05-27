c129f203c9d6293bdd79f73c1a3afe7ab6cdefe5
차지원 <chajiwon100785@gmail.com>
2022-05-28 00:29:59 +0900

    Feat: 추가 todo 화면으로 전환 [#82]

---
+++ app/src/main/java/com/wap/storemanagement/ui/todo/ToDoFragment.kt
@@ -0,0 +1,9 @@
+package com.wap.storemanagement.ui.todo
+
+import com.wap.base.BaseFragment
+import com.wap.storemanagement.R
+import com.wap.storemanagement.databinding.FragmentTodoBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ToDoFragment : BaseFragment<FragmentTodoBinding>(R.layout.fragment_todo)
