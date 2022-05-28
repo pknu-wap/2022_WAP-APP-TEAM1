121b3734ba7e07338cd3c664db0bbf28ed8d5537
Daaeeuun <eun000221@gmail.com>
2022-05-28 15:43:38 +0900

    Feat: 수정 reviewdog #74

--- data/src/main/java/com/wap/data/db/TodoDatabase.kt
+++ data/src/main/java/com/wap/data/db/TodoDatabase.kt
@@ -6,6 +6,6 @@ import com.wap.data.db.dao.TodoDao
 import com.wap.domain.entity.ToDo
 
 @Database(entities = arrayOf(ToDo::class), version = 1)
-abstract class TodoDatabase: RoomDatabase() {
+abstract class TodoDatabase : RoomDatabase() {
     abstract fun TodoDao(): TodoDao
 }
