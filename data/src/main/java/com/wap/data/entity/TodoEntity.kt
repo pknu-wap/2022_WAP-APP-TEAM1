b615c25ebcfd29275db6022408f523c153a667f0
Daaeeuun <eun000221@gmail.com>
2022-05-28 13:38:14 +0900

    Feat: TodoEntity 수정 #74

--- data/src/main/java/com/wap/data/entity/TodoEntity.kt
+++ data/src/main/java/com/wap/data/entity/TodoEntity.kt
@@ -9,6 +9,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "ToDo")
 data class TodoEntity (
     @PrimaryKey(autoGenerate = true) val toDoId: Long = 0,
+    @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "contents") val contents: String,
     @ColumnInfo(name = "isComplete") var isComplete: Boolean
     )
