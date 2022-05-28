aa88e6ef2219879bcc40fd1d6b69de50a645547c
차지원 <chajiwon100785@gmail.com>
2022-05-29 01:11:11 +0900

    Feat: 추가 salary fragment [#84]

---
+++ app/src/main/java/com/wap/storemanagement/ui/salary/SalaryFragment.kt
@@ -0,0 +1,9 @@
+package com.wap.storemanagement.ui.salary
+
+import com.wap.base.BaseFragment
+import com.wap.storemanagement.R
+import com.wap.storemanagement.databinding.FragmentSalaryBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SalaryFragment : BaseFragment<FragmentSalaryBinding>(R.layout.fragment_salary)
