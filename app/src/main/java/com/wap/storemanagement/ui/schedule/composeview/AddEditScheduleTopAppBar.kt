acc7f8fe6bc777cb60fdafc5cce2ca18ed42cfb4
Kongbee <rhdmsqls5@gmail.com>
2022-05-30 23:59:51 +0900

    Layout: 추가 DeleteButton - AddEditTopAppBar [#102]

--- app/src/main/java/com/wap/storemanagement/ui/schedule/composeview/AddEditScheduleTopAppBar.kt
+++ app/src/main/java/com/wap/storemanagement/ui/schedule/composeview/AddEditScheduleTopAppBar.kt
@@ -1,23 +1,56 @@
 package com.wap.storemanagement.ui.schedule.composeview
 
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIos
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.wap.storemanagement.R
-import com.wap.storemanagement.ui.basecomposeview.BaseTopAppBar
+import com.wap.storemanagement.ui.schedule.DeleteButtonState
 
 @Composable
-fun AddEditScheduleTopAppBar() {
+fun AddEditScheduleTopAppBar(deleteButtonState: DeleteButtonState) {
+    val backgroundColor = colorResource(id = R.color.schedule_top_appbar_background)
 
-    BaseTopAppBar(
-        text = stringResource(R.string.schedule_top_appbar_title),
-        onClick = { /*TODO*/ },
-        actions = { /* TODO: 일정 체크 -> 삭제 버튼 나타남 */ }
+    TopAppBar (
+        title = { Text(text = stringResource(R.string.schedule_top_appbar_title)) },
+        backgroundColor = backgroundColor,
+        navigationIcon = {
+            IconButton(onClick = {  }) {
+                Icon(Icons.Default.ArrowBackIos, "ArrowBack")
+            }
+        },
+        actions = {
+            DeleteButton(deleteButtonState = deleteButtonState)
+        }
     )
+}
+
+@Composable
+private fun DeleteButton(deleteButtonState: DeleteButtonState) {
+    when (deleteButtonState) {
+        DeleteButtonState.ON ->
+            IconButton(
+                onClick = {},
+                content = { Icon(imageVector = Icons.Default.Delete, contentDescription = "") }
+            )
+        DeleteButtonState.OFF ->
+            IconButton(
+                onClick = { /* Nothing todo */ },
+                enabled = false,
+                content = { }
+            )
+    }
 }
 
 @Preview
 @Composable
 fun PreviewAddScheduleTopAppBar() {
-    AddEditScheduleTopAppBar()
+    AddEditScheduleTopAppBar(DeleteButtonState.OFF)
 }
