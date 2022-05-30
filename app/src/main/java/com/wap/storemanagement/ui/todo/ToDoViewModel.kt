c2ed458b413be2268aa65bf08061939205f3977c
차지원 <chajiwon100785@gmail.com>
2022-05-30 14:51:05 +0900

    Feat: 추가 todo viewmodel 입력, 출력기능 [#98]

--- app/src/main/java/com/wap/storemanagement/ui/todo/ToDoViewModel.kt
+++ app/src/main/java/com/wap/storemanagement/ui/todo/ToDoViewModel.kt
@@ -3,23 +3,44 @@ package com.wap.storemanagement.ui.todo
 import androidx.lifecycle.viewModelScope
 import com.wap.base.BaseViewModel
 import com.wap.base.provider.DispatcherProvider
-import com.wap.data.db.dao.TodoDao
-import com.wap.data.toToDo
+import com.wap.domain.datasource.ToDoDataSource
 import com.wap.domain.entity.ToDo
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class ToDoViewModel @Inject constructor(
     dispatcherProvider: DispatcherProvider,
-    private val todoDao: TodoDao
+    private val toDoDataSource: ToDoDataSource
 ): BaseViewModel(dispatcherProvider) {
 
-    val toDoList: StateFlow<List<ToDo>> = todoDao.fetchToDoList()
-        .map { todoEntityList -> todoEntityList.map { entity -> entity.toToDo() } }
+    private var _toDoList: StateFlow<List<ToDo>> = flowOf(listOf(ToDo.defaultValue))
         .stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
+    val toDoList: StateFlow<List<ToDo>> = _toDoList
+
+    var inputContents = ""
+
+    init {
+        fetchAllToDoList()
+    }
+
+    fun fetchAllToDoList() = onIo {
+        _toDoList = toDoDataSource.fetchAllToDoList()
+            .stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
+    }
+
+    fun createToDo() = onIo {
+        toDoDataSource.createToDo(
+            ToDo(
+                toDoId = 1L,
+                title = inputContents,
+                contents = inputContents,
+                isComplete = false
+            )
+        )
+    }
 }
