0df384323145c197721741b7d2d59d6389a5ac69
차지원 <chajiwon100785@gmail.com>
2022-05-30 14:30:15 +0900

    Feat: 변경 todo db 상호작용의 반환값을 flow로 교체 [#98]

--- domain/src/main/java/com/wap/domain/datasource/ToDoDataSource.kt
+++ domain/src/main/java/com/wap/domain/datasource/ToDoDataSource.kt
@@ -1,14 +1,15 @@
 package com.wap.domain.datasource
 
 import com.wap.domain.entity.ToDo
+import kotlinx.coroutines.flow.Flow
 
 interface ToDoDataSource {
 
     fun createToDo(toDo: ToDo)
 
-    fun fetchAllToDoList(): List<ToDo>
+    fun fetchAllToDoList(): Flow<List<ToDo>>
 
-    fun fetchToDoListById(toDoId: Long): ToDo
+    fun fetchToDoListById(toDoId: Long): Flow<ToDo>
 
     fun updateToDo(toDo: ToDo)
 
