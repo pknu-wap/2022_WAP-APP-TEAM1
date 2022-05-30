a8a30869e28188ccf322f04dc96fe8e68255e1eb
차지원 <chajiwon100785@gmail.com>
2022-05-30 14:50:33 +0900

    Feat: 추가 todo default value [#98]

--- domain/src/main/java/com/wap/domain/entity/ToDo.kt
+++ domain/src/main/java/com/wap/domain/entity/ToDo.kt
@@ -5,4 +5,15 @@ data class ToDo(
     val title: String,
     val contents: String,
     val isComplete: Boolean,
-)
+) {
+
+    companion object {
+
+        val defaultValue = ToDo(
+            toDoId = 1L,
+            title = "할 일을 입력해주세요",
+            contents = "할 일을 입력해주세요",
+            isComplete = false
+        )
+    }
+}
