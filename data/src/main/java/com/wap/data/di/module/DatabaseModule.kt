9004a7a3887cf8a870a01a7e4e2dfd614d403fb6
Kongbee <rhdmsqls5@gmail.com>
2022-06-03 01:03:42 +0900

    Feat: 수정 style guide

--- data/src/main/java/com/wap/data/di/module/DatabaseModule.kt
+++ data/src/main/java/com/wap/data/di/module/DatabaseModule.kt
@@ -3,19 +3,12 @@ package com.wap.data.di.module
 import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.room.Room
-import androidx.room.RoomDatabase
-import androidx.sqlite.db.SupportSQLiteDatabase
 import com.wap.data.db.AppDatabase
-import com.wap.data.entity.UserEntity
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Singleton
 
 @RequiresApi(Build.VERSION_CODES.O)
